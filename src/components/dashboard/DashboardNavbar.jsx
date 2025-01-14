23be1b677784d4292e22480380bd73151a27d3eb
tannaznamdar <tannaznamdar99@gmail.com>
2025-01-14 18:47:55 +0330

    add  Logout button

--- src/components/dashboard/DashboardNavbar.jsx
+++ src/components/dashboard/DashboardNavbar.jsx
@@ -1,8 +1,9 @@
 import propTypes from "prop-types";
 import { useEffect } from "react";
 
-import { ChevronDownIcon, BellAlertIcon, MagnifyingGlassIcon, Bars3Icon, MoonIcon } from "@heroicons/react/24/outline";
+import { ArrowRightStartOnRectangleIcon, BellAlertIcon, MagnifyingGlassIcon, Bars3Icon, MoonIcon } from "@heroicons/react/24/outline";
 import userImg from "./../../assets/image/user.png";
+import { NavLink } from "react-router";
 
 function DashboardNavbar({ openMenu }) {
     
@@ -56,16 +57,18 @@ function DashboardNavbar({ openMenu }) {
         <span className="border-r-2 md:flex hidden">
           <BellAlertIcon className="size-6 text-gray-400 md:mx-4 mx-2 transition duration-150 hover:text-gray-600 dark:hover:text-gray-300 dark:text-white" />
         </span>
-        <figure className="w-9 h-9 mx-4 rounded-full overflow-hidden flex-shrink-0">
+        <figure className="w-9 h-9 mx-4 rounded-full overflow-hidden hidden md:flex flex-shrink-0">
           <img src={userImg} alt="user" />
         </figure>
 
-        <div className="flex items-center">
-          <span className=" pr-3 font-semibold text-sm text-gray-700 dark:text-white">
-            Tom Cook
-          </span>
-          <ChevronDownIcon className="size-4 text-gray-400 transition duration-150 hover:rotate-180 hover:text-gray-600 dark:text-white dark:hover:dark:text-white" />
-        </div>
+        <button>
+          <NavLink className="flex items-center" to='/'>
+            <span className=" ml-2 pr-3 font-semibold text-sm text-gray-700 dark:text-white">
+              Logout
+            </span>
+            <ArrowRightStartOnRectangleIcon className="size-5 text-gray-400 transition duration-150 rotate-180 hover:text-gray-600 dark:text-white dark:hover:dark:text-white" />
+          </NavLink>
+        </button>
       </div>
     </div>
   );
