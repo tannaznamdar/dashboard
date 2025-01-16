3f8f3d5749dc2b936fec74534acdc8e0c0525154
tannaznamdar <tannaznamdar99@gmail.com>
2025-01-16 11:28:31 +0330

    add todo in api

--- src/components/todoes/NewTodoInput.jsx
+++ src/components/todoes/NewTodoInput.jsx
@@ -1,34 +1,40 @@
-import UseFormInput from "../../hooks/UseFormInput"
-import { v4 as uuidv4 } from 'uuid';
+import axios from "axios";
+import UseFormInput from "../../hooks/UseFormInput";
 
-export default function NewTodoInput({toDoes,setToDoes}) {
+export default function NewTodoInput({ toDoes, setToDoes }) {
+  const newTodo = UseFormInput("");
 
-    const newTodo = UseFormInput ('')
+  const addTodoHandler = async (event) => {
+    if (event.key === "Enter" && event.target.value != "") {
+      let newTodoValue = { title: event.target.value, status: false };
 
-    const addTodoHandler = (event) =>{
-        if (event.key === 'Enter' && event.target.value != ''){
+      try {
+        const res = await axios.post(
+          "https://675bf8c89ce247eb19380ed6.mockapi.io/todoes-new",
+          newTodoValue
+        );
 
-            let newTodoValue = {id:uuidv4(), title:event.target.value, status:false }
+        setToDoes([...toDoes, { id: res.data.id, ...newTodoValue }]);
 
-            setToDoes([
-                ...toDoes,
-                newTodoValue
-            ]);
-
-            newTodo.reset();
-        }
+        newTodo.reset();
+        
+      } catch (error) {
+        console.error('Error saving todo:', error);
+        alert('مشکلی در ذخیره‌سازی تسک به وجود آمد.');
+      }
     }
+  };
 
-    return (
-
-        <div className="relative">
-            <input type="text" placeholder="What needs to be done today?"
-                className="w-full px-2 py-3 border rounded outline-none border-grey-600"
-                value={newTodo.value}
-                onChange={newTodo.onChange}
-                onKeyDown={addTodoHandler}
-                />
-        </div>
-
-    )
+  return (
+    <div className="relative">
+      <input
+        type="text"
+        placeholder="What needs to be done today?"
+        className="w-full px-2 py-3 border rounded outline-none border-grey-600"
+        value={newTodo.value}
+        onChange={newTodo.onChange}
+        onKeyDown={addTodoHandler}
+      />
+    </div>
+  );
 }
--- src/components/todoes/ToDoes.jsx
+++ src/components/todoes/ToDoes.jsx
@@ -1,54 +1,55 @@
 import { useState } from "react";
 import NewTodoInput from "./NewTodoInput";
 import ToDoList from "./ToDoList";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
-export default function ToDoes(){
+export default function ToDoes() {
+  const [toDoes, setToDoes] = useState([]);
 
-    const [toDoes, setToDoes] = useState ([
-        {id:uuidv4() , title: 'go to school', status : false},
-        {id:uuidv4() , title: 'go to game', status : true}
-    ]) 
+  const deleteTodoHandler = (todo) => {
+    let newTodoes = toDoes.filter((item) => {
+      return todo.id != item.id;
+    });
+    setToDoes(newTodoes);
+  };
 
-    const deleteTodoHandler =(todo)=>{
-        let newTodoes = toDoes.filter((item)=>{
-            return todo.id != item.id ;
-        })
-        setToDoes(newTodoes)
-    }
+  const toggleTodoStatusHandler = (todo) => {
+    let newTodoes = toDoes.map((item) => {
+      if (item.id === todo.id) {
+        item.status = !item.status;
+      }
+      return item;
+    });
+    setToDoes(newTodoes);
+  };
 
-    const toggleTodoStatusHandler =(todo)=>{
-        let newTodoes = toDoes.map ((item)=>{
-            if (item.id === todo.id){
-                item.status =! item.status
-            }
-            return item
-        })
-        setToDoes(newTodoes)
-    }
+  const editTodoHandler = (newTodoTitle, todo) => {
+    let newTodoes = toDoes.map((item) => {
+      if (item.id === todo.id) {
+        todo.title = newTodoTitle;
+      }
+      return item;
+    });
+    setToDoes(newTodoes);
+  };
 
-    const editTodoHandler =(newTodoTitle, todo)=>{
-     let newTodoes = toDoes.map((item)=>{
-        if(item.id === todo.id){
-            todo.title = newTodoTitle
-        }
-        return item;
-     })
-     setToDoes(newTodoes)
-    }
-
-    return(
-
-        <div className="flex items-center justify-center py-16">
-            <div className="w-full px-4 py-8 mx-auto shadow lg:w-2/3  max-h-screen overflow-auto bg-white">
-
-                <div className="flex items-center mb-6">
-                    <h1 className="mr-6 text-4xl font-bold text-purple-600"> TO DO APP </h1>
-                </div>
-
-                <NewTodoInput toDoes={toDoes} setToDoes={setToDoes} />
-                <ToDoList toDoes={toDoes} editTodo ={editTodoHandler} deleteTodo ={deleteTodoHandler} toggleStatus={toggleTodoStatusHandler} />
-            </div>
+  return (
+    <div className="flex items-center justify-center py-16">
+      <div className="w-full px-4 py-8 mx-auto shadow lg:w-2/3  max-h-screen overflow-auto bg-white">
+        <div className="flex items-center mb-6">
+          <h1 className="mr-6 text-4xl font-bold text-purple-600">
+            TO DO APP
+          </h1>
         </div>
-    )
+
+        <NewTodoInput toDoes={toDoes} setToDoes={setToDoes} />
+        <ToDoList
+          toDoes={toDoes}
+          editTodo={editTodoHandler}
+          deleteTodo={deleteTodoHandler}
+          toggleStatus={toggleTodoStatusHandler}
+        />
+      </div>
+    </div>
+  );
 }
