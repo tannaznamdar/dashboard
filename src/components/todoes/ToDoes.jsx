159470e47829416107413eb96678f83aa173b739
tannaznamdar <tannaznamdar99@gmail.com>
2025-01-16 11:46:51 +0330

    get todoes data from api

--- src/components/todoes/ToDoes.jsx
+++ src/components/todoes/ToDoes.jsx
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import NewTodoInput from "./NewTodoInput";
 import ToDoList from "./ToDoList";
-import { v4 as uuidv4 } from "uuid";
 
 export default function ToDoes() {
   const [toDoes, setToDoes] = useState([]);
+
+  const getTodoes = async () => {
+    try {
+      const res = await axios.get(
+        "https://675bf8c89ce247eb19380ed6.mockapi.io/todoes-new"
+      );
+      setToDoes(res.data);
+    } catch (error) {
+      console.error('Error fetching todos:', error);
+      alert("مشکلی در ذخیره‌سازی تسک به وجود آمد.");
+    }
+  };
+
+  useEffect(()=>{
+    getTodoes()
+  },[])
 
   const deleteTodoHandler = (todo) => {
     let newTodoes = toDoes.filter((item) => {
@@ -37,9 +53,7 @@ export default function ToDoes() {
     <div className="flex items-center justify-center py-16">
       <div className="w-full px-4 py-8 mx-auto shadow lg:w-2/3  max-h-screen overflow-auto bg-white">
         <div className="flex items-center mb-6">
-          <h1 className="mr-6 text-4xl font-bold text-purple-600">
-            TO DO APP
-          </h1>
+          <h1 className="mr-6 text-4xl font-bold text-purple-600">TO DO APP</h1>
         </div>
 
         <NewTodoInput toDoes={toDoes} setToDoes={setToDoes} />
