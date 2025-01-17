76c20ef07767aca8cd0e09dbc9a841850ed6a515
tannaznamdar <tannaznamdar99@gmail.com>
2025-01-17 23:04:34 +0330

    router route correction

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import AppRouter from "./routes/dashboard/index.jsx";
+import AppRouter from "./routes/index.jsx";
 import { RouterProvider } from "react-router-dom";
-import React from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,27 +1,27 @@
 import { createBrowserRouter } from "react-router";
 
-import HomePage from './../../pages/HomePage'
-import Panel from "../../pages/dashboard-pages/Panel";
-import TeamItem from "../../pages/dashboard-pages/TeamItem";
-import ProjectsItem from "../../pages/dashboard-pages/ProjectsItem";
-import CalendarItem from "../../pages/dashboard-pages/CalendarItem";
-import DocumentsItem from "../../pages/dashboard-pages/DocumentsItem";
-import ReportsItem from "../../pages/dashboard-pages/ReportsItem";
-import AboutPage from "../../pages/AboutPage";
-import ToDoesPage from "../../pages/ToDoesPage";
+import HomePage from "./../pages/HomePage";
+import AboutPage from "./../pages/AboutPage";
+import ToDoesPage from "./../pages/ToDoesPage";
+import Panel from "./../pages/dashboard-pages/Panel";
+import TeamItem from "./../pages/dashboard-pages/TeamItem";
+import ProjectsItem from "./../pages/dashboard-pages/ProjectsItem";
+import CalendarItem from "./../pages/dashboard-pages/CalendarItem";
+import DocumentsItem from "./../pages/dashboard-pages/DocumentsItem";
+import ReportsItem from "./../pages/dashboard-pages/ReportsItem";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />
+    element: <HomePage />,
   },
   {
     path: "/about",
-    element: <AboutPage />
+    element: <AboutPage />,
   },
   {
     path: "/todos",
-    element: <ToDoesPage />
+    element: <ToDoesPage />,
   },
   {
     path: "/dashboard/panel",
